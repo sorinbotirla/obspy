d209526f01d668a157705478059608373efa29f8
Robert Barsch <barsch@egu.eu>
2017-02-09 12:26:48 +0100

    flake8

--- obspy/core/util/testing.py
+++ obspy/core/util/testing.py
@@ -50,6 +50,7 @@ def runTest(self):
         return vcr(self._runTest)()
     return self._runTest()
 
+
 doctest.DocTestCase._runTest = doctest.DocTestCase.runTest
 doctest.DocTestCase.runTest = runTest
 
